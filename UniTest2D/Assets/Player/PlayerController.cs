80b98ba9113e9266bd7f42d55cdd8fdd46dce522
lotco :D <kgsin7@gmail.com>
2024-01-14 05:31:07 +0900

    앞뒤 추가

--- UniTest2D/Assets/Player/PlayerController.cs
+++ UniTest2D/Assets/Player/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rbody;
     float axisH = 0.0f;
+    public float speed = 3.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +18,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         axisH = Input.GetAxisRaw("Horizontal");
+        if(axisH > 0.0f){
+            Debug.Log("right");
+            transform.localScale = new Vector2(1, 1);
+        } else if (axisH < 0.0f) {
+            Debug.Log("left");
+            transform.localScale = new Vector2(-1, 1);
+        }
     }
     
     void FixedUpdate(){
-        rbody.velocity = new Vector2(axisH * 3.0f, rbody.velocity.y);
+        rbody.velocity = new Vector2(speed * axisH, rbody.velocity.y);
     }
 }
